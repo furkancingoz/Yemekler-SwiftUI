810588dd99bb0c3b651bc331acd9e17013a7cf02
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-03-31 00:53:18 +0300

    Initial Commit

---
+++ Yemekler-SwiftUI/Yemekler_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yemekler_SwiftUIApp.swift
+//  Yemekler-SwiftUI
+//
+//  Created by Furkan Cingöz on 31.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Yemekler_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
